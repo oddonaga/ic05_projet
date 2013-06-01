3033a01aed5fafb46dba12201f56ccf035bf0297
Agathe Oddon <qryyk@yahoo.fr>
2013-06-01 19:35:55 +0200

    Nouveau Form + parser

--- form.php
+++ form.php
@@ -1,39 +1,65 @@
+<head>
+	<script>
+	function submit(){
+		document.formulaire.action = 'form.php';
+		document.formulaire.submit();
+	}
+	</script>
+</head>
+
 <?php session_start(); 
 
-// $_SESSION["id"]=0;
-echo 'id='.$_SESSION["id"];
+echo "<meta http-equiv=\"Content-Type\" content=\"text/HTML; charset=utf-8\" />";
 
-print_r($_SESSION["array"]); 
+
+// print_r($_SESSION["array"]); 
+
 
 if($_GET["choix"]){
 	$fp = fopen('data.csv', 'a');
-	fwrite($fp, "\n");
 
-	$user=$_SESSION["array"][$_SESSION["id"]]["user"];
-	$a1=explode(":",$user);
-	$a2=explode(",", $a1[1]);
+	$user=$_SESSION["array"][$_SESSION["id"]]["u"];
+	$date=$_SESSION["array"][$_SESSION["id"]]["d"];
+	$text=$_SESSION["array"][$_SESSION["id"]]["t"];
+	$choix=$_GET["choix"];
+	$session_id=$_SESSION["id"];
+
+	$_SESSION["tweets"][$session_id]=$text;
+	$_SESSION["tweets_choix"][$session_id]=$choix;
+
+	fwrite($fp, $_SESSION["id"].','.$user.','.$choix.','.$date."\n");
+	
 
 
-	fwrite($fp, $a2[0]);
+	$_SESSION["id"]=$session_id+1;
+	$text_suivant=$_SESSION["array"][$_SESSION["id"]]["t"];
+	if($key = array_search($text_suivant, $_SESSION["tweets"])){
+		// retweet -> on écrit directement dans data.csv
+		// echo '<br>RETWEET de '.$key.' : '.$_SESSION["array"][$_SESSION["id"]]["t"].'<br>user : '.$_SESSION["array"][$_SESSION["id"]]["u"].'<br>';
+		$user=$_SESSION["array"][$_SESSION["id"]]["u"];
+		$choix=$_SESSION["tweets_choix"][$key];
+		$date=$_SESSION["array"][$_SESSION["id"]]["d"];
+		fwrite($fp, $_SESSION["id"].','.$user.','.$choix.','.$date."\n");
+		$_SESSION["id"]=$_SESSION["id"]+1;
+	}
 
-	fwrite($fp, ',');
-	fwrite($fp, $_GET["choix"]);
 	fclose($fp);
-	$_SESSION["id"]=$_SESSION["id"]+1;
 }
-
 
 ?>
 
 <br><br>
 <p>
-	<?php echo $_SESSION["array"][$_SESSION["id"]]["text"]; ?>
+	<?php echo 'id='.$_SESSION["id"].'<br>';
+	echo $_SESSION["array"][$_SESSION["id"]]["t"]; ?>
 </p>
-<form action="form.php" method="GET">
-	<input type="radio" name="choix" value="pour">Pour</input>
-	<input type="radio" name="choix" value="contre">Contre</input>
-	<input type="radio" name="choix" value="neutre">Neutre</input>
-	<input type="submit" value="ok"></input>
+
+<form name="formulaire" method="GET" action="">
+	<input type="radio" name="choix" value="pour" onClick="submit();">Pour la loi</input>
+	<input type="radio" name="choix" value="contre" onClick="submit();">Contre la loi</input>
+	<input type="radio" name="choix" value="neutre" onClick="submit();">Neutre</input>
 </form>
 
 
+<!-- Format du fichier csv sortant : user_id,choix,date -->
+<!-- avec choix=pour|contre|neutre -->
---
+++ ic05_parser.php
@@ -0,0 +1,51 @@
+<?
+
+session_start();
+
+
+echo "<meta http-equiv=\"Content-Type\" content=\"text/HTML; charset=utf-8\" />";
+
+	
+// print_r($_SESSION["array"]);
+
+$array=array();
+$i=-2;
+
+$string = file_get_contents("./26mai/results.json");
+
+
+$jsonIterator = new RecursiveIteratorIterator(
+    new RecursiveArrayIterator(json_decode($string, TRUE)),
+    RecursiveIteratorIterator::SELF_FIRST);
+
+foreach ($jsonIterator as $key => $val) {
+	    if(is_array($val)) {
+	        // echo "key=".$key.":<br><br>";
+	        $i++;
+	        $array[$i]=array();
+	    }else{
+	        // echo $key." => ".$val."<br><br>";
+	        if($key=="from_user_id") $array[$i]["u"]=$val;
+	        else if($key=="text") $array[$i]["t"]=$val;
+	        else if($key=="created_at") $array[$i]["d"]=$val;
+	    }
+}
+
+
+
+
+$_SESSION["array"]=$array;
+$_SESSION["id"]=0;
+$_SESSION["tweets"]=array();
+$_SESSION["tweets_choix"]=array();
+
+echo "_SESSION[id]=".$_SESSION["id"]."<br><br>";
+
+echo "_SESSION[array]=<br>";
+print_r($array);
+
+// u : from_user_id
+// t : text
+// d : created_at
+
+?>
